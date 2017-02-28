04a4b69d605b76f6246e46445e13bad47b814efc
brendand15 <brendan.deffense@gmail.com>
2017-02-27 19:26:10 -0700

    working interrupt
    Interrupts are now working correctly.

--- Project_7Rebuild/Sources/main.asm
+++ Project_7Rebuild/Sources/main.asm
@@ -40,9 +40,12 @@ OC7D_IN     EQU   $0C   ; Same as above, needed for same reason.
 TSCR2_IN	  EQU		$02		; disable TOI, prescale = 4
 TCTL2_IN	  EQU		$A0		; Tell ch2 and ch3 to go to 0 at end of pulse length
 TCTL1_IN    EQU   $40   ; upon compare, channel 7 will be set high (which will set ch2 and 3 high)
+TCTL4_IN    EQU   $05   ; Mask for setting ch0 and ch1 to look for any rising edge.
 TIOS_IN		  EQU		$8C		; select Ch 7,2 and 3 for OC, everything else is set for input compare
 TSCR_IN 	  EQU		$80		; enable timer, normal flag CLR.
-TFLG1_Chk   EQU   $8C   ; check if ch7 matches free running counter
+TFLG1_Chk   EQU   $8F   ; check if ch7 matches free running counter
+TFLG1_Clr   EQU   $8F   ; Clear interrupts set for channels 0-3 and ch 7
+TIE_IN      EQU   $03   ; Mask to turn on interrupts for channels 0 and 1.
 PERCENT		  EQU		$80
 MAXPERCENT	EQU		$3333
 init_perc	  EQU		$0CCD	; 3,277 ticks, or 5% pulse width
@@ -93,8 +96,15 @@ mainLoop:
 ;---------------------------------
 
 			  BSR		TIMERINIT_OC2	; Timer initialization subr
-DONE	  BSR		SQ_WAVE		; square wave gen subr
-			  BRA		DONE		; Branch to self
+			  
+			  LDD	  pulse_width		;add hex value to high count, This code will create the first pulse duty cycle
+    		STD 	TC2Hi				;setup next transition time for ch 2
+    		STD   TC3Hi       ;setup next transition time for ch 3
+			  MOVB  #TIE_IN, TIE  ;Turn on interrupts for ch0 and ch1
+			  
+			  ;BSR  LCD_Subroutine_Here
+			  
+DONE	  BRA		DONE		; Branch to self
 
 ;-------------------------------------
 ;Subroutine TIMERINIT: Initialize timer for OC2
@@ -102,6 +112,7 @@ DONE	  BSR		SQ_WAVE		; square wave gen subr
 TIMERINIT_OC2	CLR		TIE 				;disable interrupts in Timer Interrupt Enable Register
 				MOVB	#TSCR2_IN,TSCR2 	;Timer System Control Register 2
 				MOVB	#TCTL2_IN,TCTL2 	;OC2 toggle on compare
+				MOVB  #TCTL4_IN,TCTL4   ;Set ch0 and ch1 interrupts to activate on any rising edge.
         MOVB  #OC7M_IN,OC7M
         MOVB  #OC7D_IN,OC7D
 				MOVB	#TIOS_IN,TIOS 		;select ch2 for OC
@@ -109,105 +120,99 @@ TIMERINIT_OC2	CLR		TIE 				;disable interrupts in Timer Interrupt Enable Registe
 				MOVB	#TSCR_IN,TSCR1		;enable timer, std flag clr4,
 				RTS							;return from subroutine
 
-;----------------------------------
-;CLEARFLG
-;Clear C@F flag by reading TFLG1 when C2F set and then writing 1 to C2F
-;----------------------------------
-CLEARFLG	LDAA	TFLG1				;Read flag first
-			    ORAA	#$80	;write 1 to flag for clearing
-			    STAA	TFLG1
-			    
-			    BRA SPEED_CTRL
 
 ;-----------------------------------
-;Subroutine INC_OF
-;increment overflow counter
-;-----------------------------------v			
+;Ch0 Interrupt code
+;-----------------------------------
+rti_intCh0: 
+        	LDAA	TFLG1				;Read flag first
+    			ORAA	#TFLG1_Clr	;write 1 to flag for clearing ch 0-3 and ch 7
+    			STAA	TFLG1   		
+    			BRA SPEED_CTRL
+          
+
+    ;-----------------------------------
+    ;Subroutine INC_OF
+    ;increment overflow counter
+    ;-----------------------------------v			
 INC_OF		LDX		o_count				;increment number of overflows by 1
-			    INX
-			    LDD   o_count
-			    SUBD  count_max
-			    BEQ   RES_COUNT
-			    STX   o_count
-			    
-		      BRA	SQ_WAVE
-RES_COUNT CLR o_count
-          BRA SQ_WAVE
+    			INX
+    			STX   o_count
+    			    
+    		  BRA EndIntr
 
-
-;-----------------------------------
-;Subroutine SQWAVE
-;-----------------------------------
-SQ_WAVE 	BRCLR	TFLG1,$80,SQ_WAVE 	;Poll for Ch7 Flag
-    			LDD	  pulse_width		;add hex value to high count
-    			STD 	TC2Hi				;setup next transition time for ch 2
-    			STD   TC3Hi       ;setup next transition time for ch 3
-    			BSR		CLEARFLG			;generate repetitive signal
-    			RTS
-
-;-----------------------------------
-;Subroutine Increase speed
-;     min + (overflow * slope)
-;-----------------------------------
+    ;-----------------------------------
+    ;Subroutine Increase speed
+    ;     min + (overflow * slope)
+    ;-----------------------------------
 inc_speed	LDD		pulse_width			;loads previous pulse width
-    			ADDD	#slope					;increment pulse_width by the slope
-    			STD		pulse_width				;save our new pulse_width value
-    			BRA		INC_OF
+        	ADDD	#slope					;increment pulse_width by the slope
+        	STD		pulse_width				;save our new pulse_width value
+        	BRA		INC_OF
 
-;-----------------------------------
-;Subroutine to decrease motor speed
-;     20% dutycycle ticks - (overflow - 10sec) * slope
-;-----------------------------------
+    ;-----------------------------------
+    ;Subroutine to decrease motor speed
+    ;     20% dutycycle ticks - (overflow - 10sec) * slope
+    ;-----------------------------------
 dec_speed	LDD		pulse_width			;loads previous pulse width
-    			SUBD	#slope					;decrement pulse_width by the slope
-    			STD		pulse_width				;save our new pulse_width value
-    			BRA		INC_OF
+        	SUBD	#slope					;decrement pulse_width by the slope
+        	STD		pulse_width				;save our new pulse_width value
+        	BRA		INC_OF
 
-;-----------------------------------
-;Subroutine to maintain top speed (%20 duty cycle) for 5 seconds
-;-----------------------------------
-top_speed 	LDD 	#init_max				;set pulse_width to init_max(20%)
-      			STD 	pulse_width				;save pulse_width value
-      			BRA		INC_OF
-      			
-      			
-;-----------------------------------
-;Subroutine to maintain min speed (%5 duty cycle) for a long time
-;-----------------------------------
-min_speed 	LDD 	#init_perc				;set pulse_width to init_min(5%)
-      			STD 	pulse_width				;save pulse_width value
-      			BRA		INC_OF
+    ;-----------------------------------
+    ;Subroutine to maintain top speed (%20 duty cycle) for 5 seconds
+    ;-----------------------------------
+top_speed LDD 	#init_max				;set pulse_width to init_max(20%)
+          STD 	pulse_width				;save pulse_width value
+          BRA		INC_OF
+          			
+          			
+    ;-----------------------------------
+    ;Subroutine to maintain min speed (%5 duty cycle) for a long time
+    ;-----------------------------------
+min_speed LDD 	#init_perc				;set pulse_width to init_min(5%)
+          STD 	pulse_width				;save pulse_width value
+          BRA		INC_OF
 
-;-----------------------------------
-;Subroutine SPEED_CTRL
-;-----------------------------------
-SPEED_CTRL  LDAA	TFLG1				;Read flag first
-			      ORAA	#$0C				;write 1 to flag for clearing
-			      STAA	TFLG1
-			
-		    	  LDD		o_count
-			      SUBD	#five_sec
-			      BLT		inc_speed			;branch if <5s
-			      
-			      LDD		o_count
-			      SUBD	#fift_sec
-			      BGT		min_speed			;branch if >15s
-			
-			      LDD		o_count
-			      SUBD	#ten_sec
-			      BGT		dec_speed			;branch if >10s
-			
-			      BRA top_speed
-
-
-		  	  
-		  	  
-		  	  
-      			END
-
+    ;-----------------------------------
+    ;Subroutine SPEED_CTRL
+    ;-----------------------------------
+SPEED_CTRL  LDD		o_count
+    			  SUBD	#five_sec
+    			  BLT		inc_speed			;branch if <5s
+    			      
+    			  LDD		o_count
+    			  SUBD	#fift_sec
+    			  BGT		min_speed			;branch if >15s
+    			
+    			  LDD		o_count
+    			  SUBD	#ten_sec
+    			  BGT		dec_speed			;branch if >10s
+    			
+    			  BRA top_speed
+    	
+    	;Subroutunne EndIntr
+    	; Call this subroutine to end the Interrupt.		  
+    			  
+    			  
+EndIntr   LDD	  pulse_width		;add hex value to high count
+          STD 	TC2Hi				;setup next transition time for ch 2
+        	STD   TC3Hi       ;setup next transition time for ch 3
+        	
+        	RTI   	
+            
+;------------------------------      			
+;End of Ch0 interrupt code     
+;------------------------------       
 
 ;**************************************************************
 ;*                 Interrupt Vectors                          *
 ;**************************************************************
+
             ORG   $FFFE
             DC.W  Entry           ; Reset Vector
+
+            ORG   Vtimch0
+            DC.W  rti_intCh0
+
+            END
